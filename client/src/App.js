443edb7aa25ccf1b47780a7de7bb2119d67021a2
Angela Xu <angela.xu.dev@gmail.com>
2024-12-21 20:05:40 -0500

    added imgs

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import './App.css';
+import OverlayBox from './OverlayBox';
 
 function App() {
   return (
@@ -7,8 +8,15 @@ function App() {
         <img src="/imgs/chimney.png" alt="Chimney" className="" />
         <div className="px-3">
           <div className="p-6 outline outline-4 outline-primarygreen flex flex-col">
-            <img src="/imgs/logo.png" />
-            <div className="grid grid-cols-3"></div>
+            <div className="grid grid-cols-3 gap-6">
+              <OverlayBox dayNumber="1"></OverlayBox>
+              <OverlayBox dayNumber="2"></OverlayBox>
+              <OverlayBox dayNumber="3"></OverlayBox>
+              <OverlayBox dayNumber="4"></OverlayBox>
+              <OverlayBox dayNumber="5"></OverlayBox>
+              <OverlayBox dayNumber="6"></OverlayBox>
+              <OverlayBox dayNumber="7"></OverlayBox>
+            </div>
           </div>
         </div>
         <img src="/imgs/lobby.png" alt="Lobby" className="" />
